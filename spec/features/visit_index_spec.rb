1b819f65be7fb57bb4c2ae0dabe97af63234c05c
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-17 11:51:20 +0000

    Added some features to homepage, need to write test for posting message

--- spec/features/visit_index_spec.rb
+++ spec/features/visit_index_spec.rb
@@ -6,8 +6,15 @@ feature "User is greeted to message board when they visit the homepage" do
 end
 
 feature "User is also presented with a message board below the greeting header" do
-  scenario "user navigates to '/'" do
+  scenario "user navigates to '/' and sees message board" do
     visit('/')
     expect(page).to have_content "Messages (most recent first):"
+  end
+end
+
+feature "User is presented with a form in order to write a message and post it" do
+  scenario "user navigates to '/' and sees form" do
+    visit('/')
+    expect(page).to have_content "Your message here"
   end
 end
